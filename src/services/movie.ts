be29192bb57299cc812f7a2b15c62b7931ef3eda
samcesa45 <samcesa45@gmail.com>
2022-12-19 19:35:29 +0100

    minor https fixes

--- src/services/movie.ts
+++ src/services/movie.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getAllMovies = async (input:string) => {
-    const response = await axios.get(`http://www.omdbapi.com/?s=${input}&apikey=${process.env.REACT_APP_API_KEY}`)
+    const response = await axios.get(`https://www.omdbapi.com/?s=${input}&apikey=${process.env.REACT_APP_API_KEY}`)
     return response
     
 
